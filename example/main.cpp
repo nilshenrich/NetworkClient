1381e429165b0e27b671e843efcacfa4ca572666
nilshenrich <nilshenrich@web.de>
2022-11-28 10:00:11 +0100

    Updated example

--- example/main.cpp
+++ example/main.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <thread>
 
 #include "NetworkClient/TcpClient.h"
 #include "NetworkClient/TlsClient.h"
@@ -19,185 +20,51 @@
 using namespace std;
 using namespace networking;
 
-// File streams for continuous stream forwarding
-ofstream StreamForward_tcp{"output_tcp.txt"};
-ofstream StreamForward_tls{"output_tls.txt"};
-
-// Example class for fragmented message transfer that derives from TcpClient and TlsClient
-class ExampleClient_fragmented : private TcpClient, private TlsClient
-{
-public:
-    // Constructor and destructor
-    ExampleClient_fragmented() : TcpClient{'\x00'}, TlsClient{'\x00'} {}
-    virtual ~ExampleClient_fragmented() {}
-
-    // Start TCP and TLS client
-    int start()
-    {
-        // Start TCP client
-        int start_tcp{TcpClient::start("localhost", 8081)};
-
-        // Start TLS client
-        int start_tls{TlsClient::start("localhost", 8082, "../keys/ca/ca_cert.pem", "../keys/client/client_cert.pem", "../keys/client/client_key.pem")};
-
-        // Return code (2 bytes): High byte: TLS, low byte: TCP
-        return (start_tls << 8) | start_tcp;
-    }
-
-    // Stop TCP and TLS client
-    void stop()
-    {
-        // Stop TCP server
-        TcpClient::stop();
-
-        // Stop TLS server
-        TlsClient::stop();
-
-        return;
-    }
-
-    // Send unencrypted data to server
-    void sendMsg_Tcp(const string &msg)
-    {
-        // Send message to TCP server
-        TcpClient::sendMsg(msg);
-
-        return;
-    }
-
-    // Send encrypted data to server
-    void sendMsg_Tls(const string &msg)
-    {
-        // Send message to TLS server
-        TlsClient::sendMsg(msg);
-
-        return;
-    }
-
-private:
-    // Override abstract methods
-    void workOnMessage_TcpClient(const std::string tcpMsgFromServer)
-    {
-        cout << "Message from TCP server: " << tcpMsgFromServer << endl;
-        return;
-    }
-
-    void workOnMessage_TlsClient(const std::string tlsMsgFromServer)
-    {
-        cout << "Message from TLS server: " << tlsMsgFromServer << endl;
-        return;
-    }
-};
-
-// Example class for continuous message transfer that derives from TcpClient and TlsClient
-class ExampleClient_continuous : private TcpClient, private TlsClient
-{
-public:
-    // Constructor and destructor
-    ExampleClient_continuous() : TcpClient{StreamForward_tcp}, TlsClient{StreamForward_tls} {}
-    virtual ~ExampleClient_continuous() {}
-
-    // Start TCP and TLS client
-    int start()
-    {
-        // Start TCP client
-        int start_tcp{TcpClient::start("localhost", 8081)};
-
-        // Start TLS client
-        int start_tls{TlsClient::start("localhost", 8082, "../keys/ca/ca_cert.pem", "../keys/client/client_cert.pem", "../keys/client/client_key.pem")};
-
-        // Return code (2 bytes): High byte: TLS, low byte: TCP
-        return (start_tls << 8) | start_tcp;
-    }
-
-    // Stop TCP and TLS client
-    void stop()
-    {
-        // Stop TCP server
-        TcpClient::stop();
-
-        // Stop TLS server
-        TlsClient::stop();
-
-        return;
-    }
-
-    // Send unencrypted data to server
-    void sendMsg_Tcp(const string &msg)
-    {
-        // Send message to TCP server
-        TcpClient::sendMsg(msg);
-
-        return;
-    }
-
-    // Send encrypted data to server
-    void sendMsg_Tls(const string &msg)
-    {
-        // Send message to TLS server
-        TlsClient::sendMsg(msg);
-
-        return;
-    }
-
-private:
-    // Override abstract methods
-    void workOnMessage_TcpClient(const std::string tcpMsgFromServer)
-    {
-        cerr << "Work on TCP message should never be executed" << endl;
-        return;
-    }
-
-    void workOnMessage_TlsClient(const std::string tlsMsgFromServer)
-    {
-        cerr << "Work on TLS message should never be executed" << endl;
-        return;
-    }
-};
+// Global functions
+void tcp_fragmented_workOnMessage(string msg) { cout << "Message from TCP server: " << msg << endl; }
+void tls_fragmented_workOnMessage(string msg) { cout << "Message from TLS server: " << msg << endl; }
 
 int main()
 {
-    // Create clients
-    ExampleClient_fragmented client_fragmented;
-    ExampleClient_continuous client_continuous;
-
-    // Start client
-    int start{client_fragmented.start() & client_continuous.start()};
-    if (start)
+    // User decision
+    while (true)
     {
-        cerr << "Error when starting client:s " << start << endl;
-        return start;
+        cout << "What mode shall be used?" << endl
+             << "    c: Continuous stream" << endl
+             << "    f: Fragmented messages" << endl
+             << "    other key: Exit program" << endl;
+        char decision;
+        cin >> decision;
+        switch (decision)
+        {
+        case 'c':
+        case 'C':
+        {
+            ofstream ofs_tcp{"MessageStream_TCP_Server", ios::app};
+            ofstream ofs_tls{"MessageStream_TLS_Server", ios::app};
+            TcpClient tcpClient{ofs_tcp};
+            TlsClient tlsClient{ofs_tls};
+            tcpClient.start("localhost", 8081);
+            tlsClient.start("localhost", 8082, "../keys/ca/ca_cert.pem", "../keys/client/client_cert.pem", "../keys/client/client_key.pem");
+            tcpClient.sendMsg("Hello TCP server! - forwarding mode");
+            tlsClient.sendMsg("Hello TLS server! - forwarding mode");
+            break;
+        }
+
+        case 'f':
+        case 'F':
+        {
+            TcpClient tcpClient{'\n', &tcp_fragmented_workOnMessage};
+            TlsClient tlsClient{'\n', &tls_fragmented_workOnMessage};
+            tcpClient.start("localhost", 8081);
+            tlsClient.start("localhost", 8082, "../keys/ca/ca_cert.pem", "../keys/client/client_cert.pem", "../keys/client/client_key.pem");
+            tcpClient.sendMsg("Hello TCP server! - fragmentation mode");
+            tlsClient.sendMsg("Hello TLS server! - fragmentation mode");
+            break;
+        }
+
+        default:
+            return 0;
+        }
     }
-
-    cout << "Clients started." << endl;
-
-    char command;
-    cout << "Please select command:" << endl
-         << "    - F: Fragmented mode" << endl
-         << "    - C: Continuous mode" << endl;
-    cin >> command;
-    switch (command)
-    {
-    case 'F':
-    case 'f':
-        client_fragmented.sendMsg_Tcp("Hello fragmented TCP server!");
-        client_fragmented.sendMsg_Tls("Hello fragmented TLS server!");
-        break;
-
-    case 'C':
-    case 'c':
-        client_continuous.sendMsg_Tcp("Hello continuous TCP server!");
-        client_continuous.sendMsg_Tls("Hello continuous TLS server!");
-    }
-
-    // Stop clients
-    client_fragmented.stop();
-    client_continuous.stop();
-
-    cout << "Clients stopped." << endl;
-
-    StreamForward_tcp.flush();
-    StreamForward_tls.flush();
-
-    return 0;
 }
